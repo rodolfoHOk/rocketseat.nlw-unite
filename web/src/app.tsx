f69b2118d9a691cf37d8e0bbaa02687e619ce2b1
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-02 21:14:49 -0300

    feat: web create header component

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,11 @@
+import { AttendeeList } from './components/attendee-list'
+import { Header } from './components/header'
+
 export function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
---
+++ web/src/components/attendee-list.tsx
@@ -0,0 +1,7 @@
+export function AttendeeList() {
+  return (
+    <div className="">
+      <p>ToDo AttendeeList</p>
+    </div>
+  )
+}
