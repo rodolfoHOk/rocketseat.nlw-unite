eb569376e9681bf216f5c30091b62ad40466c5e5
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-02 22:51:15 -0300

    feat: web create table components

--- web/src/components/attendee-list.tsx
+++ web/src/components/attendee-list.tsx
@@ -7,6 +7,10 @@ import {
   Search,
 } from 'lucide-react'
 import { IconButton } from './icon-button'
+import { Table } from './table/table'
+import { TableHeader } from './table/table-header'
+import { TableCell } from './table/table-cell'
+import { TableRow } from './table/table-row'
 
 export function AttendeeList() {
   return (
@@ -24,121 +28,91 @@ export function AttendeeList() {
         </div>
       </div>
 
-      <div className="border border-white/10 rounded-lg">
-        <table className="w-full">
-          <thead>
-            <tr className="border-b border-white/10">
-              <th
-                style={{ width: 48 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              >
+      <Table className="w-full">
+        <thead>
+          <tr className="border-b border-white/10">
+            <TableHeader style={{ width: 48 }}>
+              <input
+                type="checkbox"
+                className="size-4 bg-black/20 rounded border-white/10 text-orange-400"
+              />
+            </TableHeader>
+
+            <TableHeader>Código</TableHeader>
+
+            <TableHeader>Participante</TableHeader>
+
+            <TableHeader>Data de inscrição</TableHeader>
+
+            <TableHeader>Data do check-in</TableHeader>
+
+            <TableHeader style={{ width: 64 }}></TableHeader>
+          </tr>
+        </thead>
+
+        <tbody>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <TableRow key={index}>
+              <TableCell>
                 <input
                   type="checkbox"
                   className="size-4 bg-black/20 rounded border-white/10 text-orange-400"
                 />
-              </th>
+              </TableCell>
 
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Código
-              </th>
+              <TableCell>12345</TableCell>
 
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Participante
-              </th>
+              <TableCell>
+                <div className="flex flex-col gap-1">
+                  <span className="font-semibold text-white">Rodolfo HiOk</span>
 
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data de inscrição
-              </th>
-
-              <th className="py-3 px-4 text-sm font-semibold text-left">
-                Data do check-in
-              </th>
-
-              <th
-                style={{ width: 64 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              ></th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {Array.from({ length: 8 }).map((_, index) => (
-              <tr
-                key={index}
-                className="border-b border-white/10 hover:bg-white/5"
-              >
-                <td className="py-3 px-4 text-sm text-zinc-300">
-                  <input
-                    type="checkbox"
-                    className="size-4 bg-black/20 rounded border-white/10 text-orange-400"
-                  />
-                </td>
-
-                <td className="py-3 px-4 text-sm text-zinc-300">12345</td>
-
-                <td className="py-3 px-4 text-sm text-zinc-300">
-                  <div className="flex flex-col gap-1">
-                    <span className="font-semibold text-white">
-                      Rodolfo HiOk
-                    </span>
-
-                    <span>rudolf.hiok@email.com.br</span>
-                  </div>
-                </td>
-
-                <td className="py-3 px-4 text-sm text-zinc-300">
-                  7 dias atrás
-                </td>
-
-                <td className="py-3 px-4 text-sm text-zinc-300">
-                  3 dias atrás
-                </td>
-
-                <td className="py-3 px-4 text-sm text-zinc-300">
-                  <IconButton transparent>
-                    <MoreHorizontal className="size-4" />
-                  </IconButton>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-
-          <tfoot>
-            <tr>
-              <td colSpan={3} className="py-3 px-4 text-sm text-zinc-300">
-                Mostrando 10 de 228 items
-              </td>
-
-              <td
-                colSpan={3}
-                className="py-3 px-4 text-sm text-zinc-300 text-right"
-              >
-                <div className="inline-flex items-center gap-8">
-                  <span>Página 1 de 23</span>
-
-                  <div className="flex gap-1.5">
-                    <IconButton disabled={true}>
-                      <ChevronsLeft className="size-4" />
-                    </IconButton>
-
-                    <IconButton disabled={true}>
-                      <ChevronLeft className="size-4" />
-                    </IconButton>
-
-                    <IconButton disabled={false}>
-                      <ChevronRight className="size-4" />
-                    </IconButton>
-
-                    <IconButton disabled={false}>
-                      <ChevronsRight className="size-4" />
-                    </IconButton>
-                  </div>
+                  <span>rudolf.hiok@email.com.br</span>
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+              </TableCell>
+
+              <TableCell>7 dias atrás</TableCell>
+
+              <TableCell>3 dias atrás</TableCell>
+
+              <TableCell>
+                <IconButton transparent>
+                  <MoreHorizontal className="size-4" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <TableCell colSpan={3}>Mostrando 10 de 228 items</TableCell>
+
+            <TableCell colSpan={3} className="text-right">
+              <div className="inline-flex items-center gap-8">
+                <span>Página 1 de 23</span>
+
+                <div className="flex gap-1.5">
+                  <IconButton disabled={true}>
+                    <ChevronsLeft className="size-4" />
+                  </IconButton>
+
+                  <IconButton disabled={true}>
+                    <ChevronLeft className="size-4" />
+                  </IconButton>
+
+                  <IconButton disabled={false}>
+                    <ChevronRight className="size-4" />
+                  </IconButton>
+
+                  <IconButton disabled={false}>
+                    <ChevronsRight className="size-4" />
+                  </IconButton>
+                </div>
+              </div>
+            </TableCell>
+          </tr>
+        </tfoot>
+      </Table>
     </div>
   )
 }
---
+++ web/src/components/table/table-cell.tsx
@@ -0,0 +1,9 @@
+import { ComponentProps } from 'react'
+
+interface TableCellProps extends ComponentProps<'th'> {}
+
+export function TableCell({ className, ...rest }: TableCellProps) {
+  return (
+    <td className={`py-3 px-4 text-sm text-zinc-300" ${className}`} {...rest} />
+  )
+}
