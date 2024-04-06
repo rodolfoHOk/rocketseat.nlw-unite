252bc8ef39d61178be86a6f7a2ed34a68d67af91
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-06 00:39:56 -0300

    feat: csharp add attendees amount counter in get event by id use case

--- csharp/PassIn/PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
+++ csharp/PassIn/PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
@@ -1,18 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using PassIn.Communication.Responses;
 using PassIn.Exceptions;
 using PassIn.Infrastructure;
 
-namespace PassIn.Application.UseCases.Events.Register;
+namespace PassIn.Application.UseCases.Events.GetById;
 
 public class GetEventByIdUseCase
 {
   public ResponseEventJson Execute(Guid id)
   {
     var dbContext = new PassInDbContext();
-    var entity = dbContext.Events.Find(id);
+    var entity = dbContext.Events.Include(ev => ev.Attendees).FirstOrDefault(ev => ev.Id == id);
     
     if (entity is null) 
-      throw new NotFoundException("An event with this id dont exist.");
+      throw new NotFoundException("An event with this id don't exist.");
     
     return new ResponseEventJson
     {
@@ -20,7 +21,7 @@ public class GetEventByIdUseCase
       Title = entity.Title,
       Details = entity.Details,
       MaximumAttendees = entity.Maximum_Attendees,
-      AttendeesAmount = -1
+      AttendeesAmount = entity.Attendees.Count()
     };
   }
 }
