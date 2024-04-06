0748774de2d78942c65eeb60003b20af454869ae
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-04-06 00:37:05 -0300

    feat: csharp add do attendees check in use case

---
+++ csharp/PassIn/PassIn.Application/UseCases/Attendees/DoCheckIn/DoAttendeeCheckInUseCase.cs
@@ -0,0 +1,45 @@
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Application.UseCases.Attendees.DoCheckIn;
+
+public class DoAttendeeCheckInUseCase
+{
+  private readonly PassInDbContext _dbContext;
+
+  public DoAttendeeCheckInUseCase()
+  {
+    _dbContext = new PassInDbContext();
+  }
+
+  public ResponseRegisteredJson Execute(Guid attendeeId)
+  {
+    Validate(attendeeId);
+
+    var entity = new CheckIn
+    {
+      Attendee_Id = attendeeId,
+      Created_at = DateTime.UtcNow
+    };
+    _dbContext.CheckIns.Add(entity);
+    _dbContext.SaveChanges();
+
+    return new ResponseRegisteredJson
+    {
+      Id = entity.Id
+    };
+  }
+
+  private void Validate(Guid attendeeId)
+  {
+    var existAttendee = _dbContext.Attendees.Any(attendee => attendee.Id == attendeeId);
+    if (existAttendee == false)
+      throw new NotFoundException("The attendee with this Id was not found.");
+
+    var existCheckIn = _dbContext.CheckIns.Any(ch => ch.Attendee_Id == attendeeId);
+    if (existCheckIn)
+      throw new ConflictException("Attendee can not do checking twice in the same event.");
+  }
+}
